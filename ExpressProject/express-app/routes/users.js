058a035ebc0c623bd90749e56f41a3422fd56238
Aleena Dilawaiz <alinadilawaiz21@gmail.com>
2025-11-22 00:08:36 +0500

    Initial commit of the ExpressProject

---
+++ ExpressProject/express-app/routes/users.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+res.render('user', {
+  title: 'Users page tutorial', 
+  message:"Users listed" });
+
+module.exports = router;
